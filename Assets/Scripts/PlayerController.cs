32666d32cd288d94450d32233a1a939833448415
Mikhail <misha-melnik-03@mail.ru>
2022-09-29 17:31:16 -0400

    Chapter 4 - Added Mouse Cursor Rotation Mechanic of the Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 50.0f;
     private CharacterController characterController;
     public Rigidbody head;
+    public LayerMask layerMask;
+    private Vector3 currentLookTarget = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -33,5 +35,28 @@ public class PlayerController : MonoBehaviour
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
         }
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            if(hit.point != currentLookTarget)
+            {
+                currentLookTarget = hit.point;
+            }
+        }
+
+        // 1
+        Vector3 targetPosition = new Vector3(hit.point.x,
+         transform.position.y, hit.point.z);
+        // 2
+        Quaternion rotation = Quaternion.LookRotation(targetPosition -
+         transform.position);
+        // 3
+        transform.rotation = Quaternion.Lerp(transform.rotation,
+         rotation, Time.deltaTime * 10.0f);
     }
 }
