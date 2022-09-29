79a7445aa09f62375d374d6211a4e7ef59becc45
Mikhail <misha-melnik-03@mail.ru>
2022-09-29 17:21:00 -0400

    Chapter 4 - Added Joints to the Marine's Head for Bouncing Effect

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
     private CharacterController characterController;
+    public Rigidbody head;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +19,19 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if(moveDirection == Vector3.zero)
+        {
+            // todo
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
     }
 }
