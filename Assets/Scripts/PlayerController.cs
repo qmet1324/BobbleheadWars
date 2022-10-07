113e592cbeafc8d0a373667e042a49a102d5175a
Mikhail <misha-melnik-03@mail.ru>
2022-10-07 03:51:09 -0400

    Chapter 6 - Animated Marine's Body

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public Rigidbody head;
     public LayerMask layerMask;
     private Vector3 currentLookTarget = Vector3.zero;
+    public Animator bodyAnimator;
 
     // Start is called before the first frame update
     void Start()
@@ -29,11 +30,13 @@ public class PlayerController : MonoBehaviour
 
         if(moveDirection == Vector3.zero)
         {
-            // todo
+            bodyAnimator.SetBool("IsMoving", false);
+
         }
         else
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
+            bodyAnimator.SetBool("IsMoving", true);
         }
 
         RaycastHit hit;
