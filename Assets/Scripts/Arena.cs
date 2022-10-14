ada7d977b966e016ee8bddaf5c71b12411264f46
Mikhail <misha-melnik-03@mail.ru>
2022-10-14 00:50:52 -0400

    Chapter 8 - Added Winning Sequence

---
+++ Assets/Scripts/Arena.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arena : MonoBehaviour
+{
+    public GameObject player;
+    public Transform elevator;
+    private Animator arenaAnimator;
+    private SphereCollider sphereCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arenaAnimator = GetComponent<Animator>();
+        sphereCollider = GetComponent<SphereCollider>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Camera.main.transform.parent.gameObject.
+        GetComponent<CameraMovement>().enabled = false;
+        player.transform.parent = elevator.transform;
+
+        player.GetComponent<PlayerController>().enabled = false;
+
+        arenaAnimator.SetBool("OnElevator", true);
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.elevatorArrived);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
